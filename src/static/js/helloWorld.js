057f6f31712e5a2ef6a2d959055b881235c1c16d
ryowuandjanet <ryowu0329@gmail.com>
2022-07-02 13:19:15 +0800

    init commit

---
+++ src/static/js/helloWorld.js
@@ -0,0 +1,10 @@
+window.onresize = screen;
+window.onload = screen;
+
+function screen() {
+  myWidth = window.innerWidth;
+
+  document.getElementById('size').innerHTML = 'Width : ' + myWidth + 'px';
+}
+
+export default helloWorld;  
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: false,
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
